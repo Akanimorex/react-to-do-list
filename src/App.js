923c0fc5ee418514f59164784b5d673668caa4a6
Akanimorex <akanimoime2016@gmail.com>
2020-05-20 05:00:52 +0100

    productivity app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import InputField from './InputField';
+import './App.css';
+
+class App extends Component{
+
+
+
+  render(){
+    return(
+      <div>
+
+        <InputField/>
+
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/InputField.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const InputField =()=>{
+    return(
+        <div>
+
+            <input type="text" placeholder="activities" />
+
+        </div>
+    )
+}
+
+export default InputField;
