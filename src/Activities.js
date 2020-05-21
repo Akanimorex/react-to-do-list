dff9e1491b45948245347caecd4a2ce48624ffe8
Akanimorex <akanimoime2016@gmail.com>
2020-05-21 13:49:17 +0100

    so far so good

--- src/Activities.js
+++ src/Activities.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import './Activities.css'
+import { Icon } from 'semantic-ui-react';
 
 
 const Activities=()=>{
     return(
-        <div>
 
-            <h1>Shut up</h1>
+
+        <div className="list">
+            <div>
+            <h3>Cook Rice</h3>
+
+            </div>
+
+
+            <div>
+
+             <Icon name="pencil"/>
+
+            </div>
+           
+           
+            
 
         </div>
     )
