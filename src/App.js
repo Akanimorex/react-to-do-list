0b3b4ca641f699cebad1a4737956aa9c1f48e12a
Akanimorex <akanimoime2016@gmail.com>
2020-05-21 13:46:54 +0100

    so good

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import InputField from './InputField';
+import Activities from './Activities';
 import './App.css';
 
 class App extends Component{
@@ -12,6 +13,7 @@ class App extends Component{
 
         <h1>To do list</h1>
         <InputField/>
+        <Activities/>
 
       </div>
     )
