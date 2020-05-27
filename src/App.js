9a89547a449c099bde312d161937a79234c5e061
Akanimorex <akanimoime2016@gmail.com>
2020-05-27 08:49:00 +0100

    love

--- src/App.js
+++ src/App.js
@@ -4,56 +4,58 @@ import Activities from "./Activities";
 import "./App.css";
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      todo:[],
-      adding:""
+     todo:[],
+     text:""
     };
 
   }
 
 
-componentDidMount(){
-  const lists =[];
-   
-
-
+onChangeAdd = (event) =>{
+  this.setState({
+    text:event.target.value
+  })
 }
 
-onSubmitAdd = (event) => {
-  if (event.charCode === 13){
-    console.log(event.target.value);
-    this.setState({todo:event.target.value})
-  } 
-};
+  onSubmitAdd = (event) => {
+    console.log('click')
+    
+      // this.setState({text:event.target.value});
+
+      const newItem = {
+        id:0,
+        text:this.state.text,
+        done:false
+      }; 
+      
+      this.setState((prevState)=>( [{
+        todo:prevState.todo.concat(newItem),
+       
+      }]))
+       
+    
+  };
 
 
 
+  render() { 
+  
+   return(
 
-
-  render() {
-    // const lists = [
-    //   {
-    //     id: 0,
-    //     todo: 'see a movie'
-    //   }, 
-    //   {
-    //     id: 1,
-    //     todo: 'go to church'
-    //   }, 
-    // ];
-    let output = this.state.todo.map(item => {
-      return (<Activities key={item.id} list={item.todo} />)
-    })
-    console.log(output)
-    return (
       <div>
         <h1>To do list</h1>
-        <InputField add={ this.onSubmitAdd } />
-        {output}
-      </div>
-    );
+        <InputField change={ this.onChangeAdd } /> <button onClick={ this.onSubmitAdd }>Add</button>
+        <Activities  todo={this.state.todo} />
+        
+    </div>
+
+   )
+    
+
+   
   }
 };
 
