82cce5f23670cdc9ca92c4d86821a90eb35dd2b1
Akanimorex <akanimoime2016@gmail.com>
2020-05-27 08:49:46 +0100

    prosperity

--- src/Activities.js
+++ src/Activities.js
@@ -4,23 +4,26 @@ import { Icon } from 'semantic-ui-react';
 
 
 const Activities=(props)=>{
+
+
+    const  lists = props.todo.map((list)=>{
+        return (
+
+            
+            <div className="list" key={list.id} >
+                <div>
+                    <h3>{list.text}</h3>
+                </div>
+                <div>
+                    <Icon name="pencil"/>
+                </div>        
+            </div>
+
+        )
+    })
     return(
 
-
-        <div className="list">
-            <div>
-                <h3>{props.list}</h3>
-            </div>
-
-
-            <div>
-                 <Icon name="pencil"/>
-            </div>
-           
-           
-            
-
-        </div>
+        <div>{lists}</div>
     )
 }
 
