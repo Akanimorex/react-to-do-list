7721e67658734eb008aec89ca94ec1ef93561c7a
Akanimorex <akanimoime2016@gmail.com>
2020-05-30 22:00:06 +0100

    list component

--- src/ListItem.js
+++ src/ListItem.js
@@ -1,20 +1,17 @@
-import React from 'react';
-import { Icon } from 'semantic-ui-react';
+import React from "react";
+import { Icon } from "semantic-ui-react";
 
-
-
-const ListItem = (props) =>{
-    return(
-        <div className="list" >
-                     <div>
-                         <h3>{props.text}</h3>
-                     </div>
-                     <div>
-                         <Icon name="pencil"/>
-                     </div>        
-                </div>
-    )
-}
-
+const ListItem = (props) => {
+  return (
+    <div className="list">
+      <div>
+        <h3>{props.text}</h3>
+      </div>
+      <div>
+        <Icon name="pencil" />
+      </div>
+    </div>
+  );
+};
 
 export default ListItem;
